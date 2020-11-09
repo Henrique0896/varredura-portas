63bc9c398ef281ac175a82821f965c45c8fff4d0
henrique0896 <barrosohenriquelima@gmail.com>
2020-11-09 11:16:51 -0300

    changing errors

--- varreduraPortas.py
+++ varreduraPortas.py
@@ -1,46 +1,40 @@
 
 from socket import gethostbyname, socket, AF_INET, SOCK_STREAM
 
-#Executa caso o programa receba um dado inesperado
-def erroEntrada():
-    print("\nEntrada inválida!\n")
-    exit()
-
-
 # ENTRADA DO IP
-#O usúario irá passar um IP ou um link para que seja feita a varredura
-#O método gethostbyname vai retornar o IP caso seja digitado um link
-#gethostbyname retorna o próprio IP se o usuário digitar um válido, se não, ele lança uma exceção 
+#O usuario ira passar um IP ou um link para que seja feita a varredura
+#O metodo gethostbyname vai retornar o IP caso seja digitado um link
+#gethostbyname retorna o proprio IP se o usuario digitar um valido, se nao, ele lanca uma excecao 
 print(" \n\tPrograma de varredura de portas abertas!")
 try:
-    dispositivo = input("Digite o IP/Domínio do host que deseja realizar a varredura: ")
+    dispositivo = input("Digite o IP/Dominio do host que deseja realizar a varredura: ")
     ip = gethostbyname(dispositivo)
-except:
-    erroEntrada()
-#Se estiver tudo ok, teremos um IP válido salvo na variável 'ip'
+except ValueError:
+      print('Erro! Verifique e tente novamente: {}'.format(ValueError))
+#Se estiver tudo ok, teremos um IP valido salvo na variavel 'ip'
 
 
 # ENTRADA DO INTERVALO DE PORTAS
-#O usúario vai digitar o intervalo de portas para a varredura
-#Caso ele não digite um número inteiro, será lançada uma exceção
-#Caso ele digite números fora do intervalo esperado, será lançada uma exceção também
+#O usuario vai digitar o intervalo de portas para a varredura
+#Caso ele nao digite um numero inteiro, sera lancada uma excecao
+#Caso ele digite numeros fora do intervalo esperado, sera lancada uma excecao tambem
 try:
     print("\nDigite o intervalo de portas que vai ser coberto:")
     portaInicial = int(input("Porta Inicial: "))
     portaFinal = int(input("Porta Final: "))
     if(portaInicial > portaFinal or portaInicial < 0 or portaFinal > 65635):
         raise Exception
-except:
-    erroEntrada()
+except ValueError:
+      print('Erro! Verifique e tente novamente: {}'.format(ValueError))
 #Se tudo estiver ok, agora temos o ip e um intervalo para fazer a varredura
 
 
-# REALIZAÇÃO DA VARREDURA
-#É criado um laço para percorrer todo o intervalo escolhido
-#A variável 'sock' recebe um novo socket com a família de endereços 'AF_INET' (Padrão)
-#O tipo de socket também é o Padrão 'SOCK_STREAM'
-#A variável 'res' conterá a resposta da conexão ao socket com o endereço (IP e Porta)
-#Se a porta estiver aberta o método 'sock.connect_ex' retornará '0'
+# REALIZACAO DA VARREDURA
+#E criado um laco para percorrer todo o intervalo escolhido
+#A variavel 'sock' recebe um novo socket com a familia de enderecos 'AF_INET' (Padrao)
+#O tipo de socket tambem sera o Padrao 'SOCK_STREAM'
+#A variavel 'res' contera a resposta da conexao ao socket com o endereco (IP e Porta)
+#Se a porta estiver aberta o metodo 'sock.connect_ex' retornara '0'
 try:
     print("\nPORTA           STATUS")
     for i in range(portaInicial, portaFinal+1):
@@ -51,5 +45,5 @@ try:
         else:
             print(" %d      ->     fechada" %(i))
     print("\nVarredura encerrada!\n")
-except:
-    print("Erro inesperado ao realizar a varredura")
+except ValueError:
+      print('Erro! Verifique e tente novamente: {}'.format(ValueError))
