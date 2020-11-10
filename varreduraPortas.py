9e01088d82e246550025165a1c9860e975d9b848
henrique0896 <barrosohenriquelima@gmail.com>
2020-11-10 15:13:22 -0300

    atualizacao

--- varreduraPortas.py
+++ varreduraPortas.py
@@ -1,16 +1,20 @@
 
 from socket import gethostbyname, socket, AF_INET, SOCK_STREAM
+from os import system
 
 # ENTRADA DO IP
 #O usuario ira passar um IP ou um link para que seja feita a varredura
 #O metodo gethostbyname vai retornar o IP caso seja digitado um link
 #gethostbyname retorna o proprio IP se o usuario digitar um valido, se nao, ele lanca uma excecao 
-print(" \n\tPrograma de varredura de portas abertas!")
 try:
+    system('clear')
+    print("Programa de varredura de portas abertas!")
     dispositivo = input("Digite o IP/Dominio do host que deseja realizar a varredura: ")
     ip = gethostbyname(dispositivo)
-except ValueError:
-      print('Erro! Verifique e tente novamente: {}'.format(ValueError))
+    system('clear')
+except:
+    print('Erro! Verifique e tente novamente!')
+    exit()
 #Se estiver tudo ok, teremos um IP valido salvo na variavel 'ip'
 
 
@@ -24,8 +28,10 @@ try:
     portaFinal = int(input("Porta Final: "))
     if(portaInicial > portaFinal or portaInicial < 0 or portaFinal > 65635):
         raise Exception
-except ValueError:
-      print('Erro! Verifique e tente novamente: {}'.format(ValueError))
+    system('clear')
+except:
+    print('Erro! Verifique e tente novamente!')
+    exit()
 #Se tudo estiver ok, agora temos o ip e um intervalo para fazer a varredura
 
 
@@ -36,14 +42,26 @@ except ValueError:
 #A variavel 'res' contera a resposta da conexao ao socket com o endereco (IP e Porta)
 #Se a porta estiver aberta o metodo 'sock.connect_ex' retornara '0'
 try:
-    print("\nPORTA           STATUS")
+    print("Varredura em andamento...")
+    portasAbertas = []
     for i in range(portaInicial, portaFinal+1):
+        print("Verificando porta: %d" %(i))
         sock = socket(AF_INET, SOCK_STREAM)
         res = sock.connect_ex((ip,  i))
         if (res == 0):
-            print(" %d      ->     ABERTA" %(i))
-        else:
-            print(" %d      ->     fechada" %(i))
-    print("\nVarredura encerrada!\n")
-except ValueError:
-      print('Erro! Verifique e tente novamente: {} '.format(ValueError))
+            portasAbertas.append(i)
+        system('clear')
+    print("Varredura encerrada!\n")
+except:
+      print('Erro! Verifique e tente novamente')
+
+#Imprimindo as portas
+print("O host: %s " %(dispositivo))
+print("Possui: %d porta(s) aberta(s)" %(len(portasAbertas)))
+print("No intervalo %d - %d" %(portaInicial, portaFinal))
+
+if( len(portasAbertas) > 0):
+    print("\nLista de Portas Abertas:")
+    for i in range( len(portasAbertas) ):
+        print(portasAbertas[i])
+print("\n")
